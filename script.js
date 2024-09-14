06e76ca95d3e7507d88c22e575ccc07acf965721
Abdullah Mohammad <abdullahmd2002@gmail.com>
2024-09-14 20:14:59 +0530

    Responsive layout

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const diceButton = document.getElementsByTagName("button")[0];
 const adviceId = document.getElementsByTagName("h1")[0];
 const adviceText = document.getElementsByTagName("p")[0];
+const divider = document.getElementsByTagName("img")[0];
 const url = "https://api.adviceslip.com/advice";
 
 const fetchAdvice = async () => {
@@ -14,3 +15,21 @@ diceButton.addEventListener("click", async () => {
     adviceId.innerText = `ADVICE #${data.slip.id}`;
     adviceText.innerText = `"${data.slip.advice}"`;
 });
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth <= 375) {
+        divider.src = "./images/pattern-divider-mobile.svg";
+    } else if (window.innerWidth > 375) {
+        divider.src = "./images/pattern-divider-desktop.svg";
+    }
+});
+
+function getCorrectDivider() {
+    if (window.innerWidth <= 375) {
+        divider.src = "./images/pattern-divider-mobile.svg";
+    } else if (window.innerWidth > 375) {
+        divider.src = "./images/pattern-divider-desktop.svg";
+    }
+}
+
+window.addEventListener("load", getCorrectDivider);
